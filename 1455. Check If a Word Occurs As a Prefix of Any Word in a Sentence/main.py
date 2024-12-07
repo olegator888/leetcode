2abb64e67484e6caf5708b9d61a0a4d1bd06b58d
olegator888 <shevchenko.code@yandex.ru>
2024-12-07 12:56:25 +0300

    1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence

---
+++ 1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence/main.py
@@ -0,0 +1,6 @@
+class Solution:
+    def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
+        words = sentence.split(" ")
+        for i, w in enumerate(words):
+            if w.startswith(searchWord): return i + 1
+        return -1
